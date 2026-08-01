5582e6ec63f0b05c36bb8c221b3d26683142845b
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-08-01 17:56:36 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 风眼 TYPHOONWATCH service worker
    Bump CACHE_VERSION on every release; it must match VERSION in index.html. */
-var CACHE_VERSION = "twatch-v1.0.0";
+var CACHE_VERSION = "twatch-v2.1.0";
 var SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", function (e) {
