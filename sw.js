c5d7fd57636a246029d057e35530abdbe271ff92
stanleywoosweeleong <130345527+stanleywoosweeleong@users.noreply.github.com>
2026-08-02 17:22:35 +0800

    bug fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 风眼 TYPHOONWATCH service worker
    Bump CACHE_VERSION on every release; it must match VERSION in index.html. */
-var CACHE_VERSION = "twatch-v3.1.12";
+var CACHE_VERSION = "twatch-v3.1.13";
 var SHELL = ["./", "./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", function (e) {
